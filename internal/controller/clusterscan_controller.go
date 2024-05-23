ccf229990b2bc9602b1968159a0dcbefd726fa9f
Visakan Kirubakaran <72267229+vikiru@users.noreply.github.com>
2024-05-22 23:49:04 -0400

    Add main controller functionality for scheduling jobs/cronjobs, deleting old jobs and logging status updates (#2)
    * Add additional type to differentiate job types
    
    * Add starting logic to create jobs/cronjobs and update status
    
    * Simplify ClusterScan spec and status
    
    * Update controller logic
    
    - keep track of active, failed, and completed jobs
    
    * Fix role perms
    
    * Update naming of jobs
    
    * Add logging message for ClusterScan status
    
    * Add functionality to cleanup old jobs and log to console
    
    * Update logging and cleanup functionality
    
    * Fix conditions for cleanup

--- internal/controller/clusterscan_controller.go
+++ internal/controller/clusterscan_controller.go
@@ -18,6 +18,15 @@ package controller
 
 import (
 	"context"
+	"fmt"
+	"strings"
+	"time"
+
+	"k8s.io/apimachinery/pkg/api/errors"
+
+	batchv1 "k8s.io/api/batch/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
@@ -36,6 +45,8 @@ type ClusterScanReconciler struct {
 //+kubebuilder:rbac:groups=webapp.clusterscan.api.io,resources=clusterscans,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=webapp.clusterscan.api.io,resources=clusterscans/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=webapp.clusterscan.api.io,resources=clusterscans/finalizers,verbs=update
+//+kubebuilder:rbac:groups=batch,resources=jobs,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=batch,resources=jobs/status,verbs=get
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
@@ -47,16 +58,188 @@ type ClusterScanReconciler struct {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.17.3/pkg/reconcile
 func (r *ClusterScanReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	log := log.FromContext(ctx)
 
-	// TODO(user): your logic here
+	// Retrieve the ClusterScan instance, output error if not found
+	var clusterScan webappv1.ClusterScan
+	if err := r.Get(ctx, req.NamespacedName, &clusterScan); err != nil {
+		log.Error(err, "unable to fetch ClusterScan")
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
+
+	// Obtain the list of all child jobs
+	var childJobs batchv1.JobList
+	if err := r.List(ctx, &childJobs, client.InNamespace(req.Namespace)); err != nil {
+		log.Error(err, "unable to list child Jobs")
+		return ctrl.Result{}, err
+	}
+
+	// Keep track of all active, successful and failed jobs then output via logger
+
+	var activeJobs []*batchv1.Job
+	var successfulJobs []*batchv1.Job
+	var failedJobs []*batchv1.Job
+
+	for i, job := range childJobs.Items {
+		_, finishType := returnJobStatus(&job)
+		switch finishType {
+		case "":
+			activeJobs = append(activeJobs, &childJobs.Items[i])
+		case batchv1.JobFailed:
+			failedJobs = append(failedJobs, &childJobs.Items[i])
+		case batchv1.JobComplete:
+			successfulJobs = append(successfulJobs, &childJobs.Items[i])
+		}
+	}
+
+	log.V(1).Info("job count", "active jobs", len(activeJobs), "successful jobs", len(successfulJobs), "failed jobs", len(failedJobs))
+
+	// Determine the type of job that the ClusterScan instance represents
+	jobType, _ := determineJobType(clusterScan)
+	uniqueName := createUniqueName(jobType, clusterScan)
+
+	// Handle recurring CronJobs
+	if jobType == webappv1.ClusterJobType("CronJob") {
+		// Create a CronJob and make the controller the owner
+		cronJob, _ := createCronJob(clusterScan, uniqueName)
+		if err := ctrl.SetControllerReference(&clusterScan, cronJob, r.Scheme); err != nil {
+			log.Error(err, "unable to set controller reference for cron job")
+			return ctrl.Result{}, err
+		}
+
+		// Check if the cron job already exists and if not, create the job
+		var cronJobInstance batchv1.CronJob
+		if err := r.Get(ctx, client.ObjectKey{Namespace: cronJob.Namespace, Name: cronJob.Name}, &cronJobInstance); err != nil {
+			if errors.IsNotFound(err) {
+				logMessage := fmt.Sprintf("Creating a new %s [%s] within %s namespace", jobType, cronJob.Name, cronJob.Namespace)
+				log.Info(logMessage)
+				if err := r.Create(ctx, cronJob); err != nil {
+					log.Error(err, "unable to create CronJob")
+					return ctrl.Result{}, err
+				}
+			}
+		}
+	} else {
+		// Handle non-recurring Jobs
+
+		// Create a Job and make the controller the owner
+		job, _ := createJob(clusterScan, uniqueName)
+		if err := ctrl.SetControllerReference(&clusterScan, job, r.Scheme); err != nil {
+			log.Error(err, "Unable to set controller reference for job")
+			return ctrl.Result{}, err
+		}
+
+		// Check if the job already exists and if not, create the job
+		var jobInstance batchv1.Job
+		if err := r.Get(ctx, client.ObjectKey{Namespace: job.Namespace, Name: job.Name}, &jobInstance); err != nil {
+			if errors.IsNotFound(err) {
+				logMessage := fmt.Sprintf("Creating a new %s [%s] within %s namespace", jobType, job.Name, job.Namespace)
+				log.Info(logMessage)
+				if err := r.Create(ctx, job); err != nil {
+					log.Error(err, "Unable to create Job")
+					return ctrl.Result{}, err
+				}
+			}
+		}
+	}
+
+	// Update the status of the ClusterScan instance and output the status info to console
+
+	clusterScan.Status.LastScheduleTime = &metav1.Time{Time: time.Now()}
+	clusterScan.Status.StatusMessage = fmt.Sprintf("A %s with the name [%s] belonging to the namespace %s has been successfully scheduled", jobType, uniqueName, clusterScan.Namespace)
+
+	if err := r.Status().Update(ctx, &clusterScan); err != nil {
+		log.Error(err, "Unable to update cluster scan status")
+		return ctrl.Result{}, err
+	}
+
+	log.V(1).Info("ClusterScan status", "status", clusterScan.Status)
+
+	// Cleanup and delete the old jobs (failed and successful)
+
+	if len(failedJobs) == 1 {
+		log.Info("Failed jobs count has reached threshold, attempting to delete old jobs.")
+		for _, job := range failedJobs {
+			if err := r.Delete(ctx, job, client.PropagationPolicy(metav1.DeletePropagationBackground)); err != nil {
+				log.Error(err, "Unable to delete old failed job")
+				return ctrl.Result{}, err
+			} else {
+				log.V(0).Info("Successfully deleted old failed job", "job", job)
+			}
+		}
+	}
+
+	if len(successfulJobs) == 3 {
+		log.Info("Successful jobs count has reached threshold, attempting to delete old jobs.")
+		for _, job := range successfulJobs {
+			if err := r.Delete(ctx, job, client.PropagationPolicy(metav1.DeletePropagationBackground)); client.IgnoreNotFound(err) != nil {
+				log.Error(err, "Unable to delete old successful job")
+			} else {
+				log.V(0).Info("Successfully deleted old successful job", "job", job)
+			}
+		}
+	}
 
 	return ctrl.Result{}, nil
+}
+
+// determineJobType returns the type of job that the ClusterScan instance represents.
+func determineJobType(clusterScan webappv1.ClusterScan) (webappv1.ClusterJobType, error) {
+	if clusterScan.Spec.Recurring {
+		return webappv1.ClusterJobType("CronJob"), nil
+	}
+	return webappv1.ClusterJobType("Job"), nil
+}
+
+// createCronJob creates a CronJob matching the metadata and spec provided by the ClusterScan instance.
+func createCronJob(clusterScan webappv1.ClusterScan, name string) (*batchv1.CronJob, error) {
+	cronJob := &batchv1.CronJob{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: clusterScan.Namespace,
+		},
+		Spec: batchv1.CronJobSpec{
+			Schedule: clusterScan.Spec.Schedule,
+			JobTemplate: batchv1.JobTemplateSpec{
+				Spec: clusterScan.Spec.JobTemplate.Spec,
+			},
+		},
+	}
+	return cronJob, nil
+}
+
+// createJob creates a Job matching the metadata and spec provided by the ClusterScan instance.
+func createJob(clusterScan webappv1.ClusterScan, name string) (*batchv1.Job, error) {
+	job := &batchv1.Job{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: clusterScan.Namespace,
+		},
+		Spec: clusterScan.Spec.JobTemplate.Spec,
+	}
+	return job, nil
+}
+
+// createUniqueName creates a unique name for the Job and CronJob matching the metadata and spec provided by the ClusterScan instance.
+func createUniqueName(jobType webappv1.ClusterJobType, clusterScan webappv1.ClusterScan) string {
+	return fmt.Sprintf("%s-%s", clusterScan.Name, strings.ToLower(string(jobType)))
+}
+
+// returnJobStatus returns the status of the Job, marking it as completed if it is completed or failed.
+func returnJobStatus(job *batchv1.Job) (bool, batchv1.JobConditionType) {
+	for _, c := range job.Status.Conditions {
+		if (c.Type == batchv1.JobComplete || c.Type == batchv1.JobFailed) && c.Status == corev1.ConditionTrue {
+			return true, c.Type
+		}
+	}
+	return false, ""
 }
 
 // SetupWithManager sets up the controller with the Manager.
 func (r *ClusterScanReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&webappv1.ClusterScan{}).
+		Owns(&batchv1.Job{}).
+		Owns(&batchv1.CronJob{}).
 		Complete(r)
 }
